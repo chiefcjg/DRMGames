29ee228122719fb9bd8477e61e864b9dc8ced9f3
ChiefCJG <chiefcjg@gmail.com>
2020-04-18 21:24:05 -0600

    kill me

---
+++ FINAL/Final Project/Assets/Scripts/Bed1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "VRplayer")
+        {
+            GameObject.Find("player").GetComponent<GameMain>().player1Ready = true;
+            Debug.Log("test");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "VRplayer")
+        {
+            GameObject.Find("player").GetComponent<GameMain>().player1Ready = false;
+            Debug.Log("test2");
+        }
+    }
+
+}
---
+++ FINAL/Final Project/Assets/Scripts/GameMain.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    //used to track when they start the game and end the game.
+    public float startTime;
+    public float endTime;
+    public bool gameRun = false;
+
+    // used to track when they are ready to load into the game.
+    public bool player1Ready = false;
+    public bool player2Ready = false;
+    public float LeverTime;
+
+    //this is used for when both levers are pulled at the same time.
+    public bool Lever1Ready = false;
+    public bool Lever2Ready = false;
+    public bool puzzle3done = false;
+
+
+    //this is used for when both pressureplates are used at the same time.
+    public bool Plate1Ready = false;
+    public bool Plate2Ready = false;
+    public bool puzzle4done = false;
+
+
+    //this is used for puzzle 5 
+    public bool Rune1 = false;
+    public bool Rune2 = false;
+    public bool Rune3 = false;
+    public bool Rune4 = false;
+    public bool Rune5 = false;
+    public bool Rune6 = false; 
+    public bool puzzle5Part1done = false;
+    public bool puzzle5Part2done = false;
+
+    //used for testing the timer remove when done testing boards and all
+    private void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            onGameStart();
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            onGameEnd();
+        }      
+    }
+
+    // will launch the game and will 
+    void onGameStart()
+    {
+        startTime = Time.time;
+        gameRun = true;
+    }
+
+    // will end the game and track end time 
+    void onGameEnd()
+    {
+        endTime = Time.time - startTime;
+        gameRun = false;
+    }
+
+    // will process the call of the levers 
+    void onLeverCheck()
+    {
+        if (Lever1Ready == true && Lever2Ready == true)
+        {
+            puzzle3done = true;
+            // insert the gameobject here and disable it from this point on/make it slide into a wall.
+        }
+    }
+    
+
+}
